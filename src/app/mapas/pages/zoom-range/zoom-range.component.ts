a12994b15953fd201636c7a2b57488a13399ee1f
Andrea Castro Manzano <andrea.castro@sofka.local>
2022-11-03 12:06:03 -0500

    Obtener las coordenadas centrales del mapa

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
 @Component({
@@ -23,14 +23,19 @@ import * as mapboxgl from 'mapbox-gl';
   `
   ]
 })
-export class ZoomRangeComponent implements AfterViewInit {
+export class ZoomRangeComponent implements AfterViewInit, OnDestroy {
 
   @ViewChild('mapa') divMapa!: ElementRef;
   mapa!: mapboxgl.Map;
   zoomLevel: number = 10;
+  center: [number, number] = [-75.48983051866664, 5.065707743505762];
 
-  constructor() { 
-    console.log('constructor', this.divMapa)
+  constructor() { }
+
+  ngOnDestroy(): void {
+    this.mapa.off('zoom', () => {});
+    this.mapa.off('zoomend', () => {});
+    this.mapa.off('move', () => {});
   }
 
   ngAfterViewInit(): void {
@@ -39,7 +44,7 @@ export class ZoomRangeComponent implements AfterViewInit {
     this.mapa = new mapboxgl.Map({
       container: this.divMapa.nativeElement, 
       style: 'mapbox://styles/mapbox/streets-v11', 
-      center: [-75.48983051866664, 5.065707743505762], 
+      center: this.center, 
       zoom: this.zoomLevel
     });
 
@@ -51,6 +56,13 @@ export class ZoomRangeComponent implements AfterViewInit {
       if(this.mapa.getZoom() > 18) {
         this.mapa.zoomTo(18);
       }
+    });
+
+    //Movimiento del mapa
+    this.mapa.on('move', (event) => {
+      const target = event.target;
+      const { lng, lat } = target.getCenter();
+      this.center = [lng, lat];
     })
   }
 
