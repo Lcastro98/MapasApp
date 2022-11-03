e78d46ffe6093076b4eabaabd017f9c722470a8e
Andrea Castro Manzano <andrea.castro@sofka.local>
2022-11-03 09:57:14 -0500

    Diseño pagina zoom-range

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,9 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
-  styles: [
+  styles: [`
+    .mapa-container {
+      width: 100%;
+      height: 100%;
+    }
+
+    .row {
+      background-color: white;
+      border-radius: 5px;
+      bottom: 50px;
+      left: 50px;
+      padding: 10px;
+      position: fixed;
+      z-index: 999;
+    }
+  `
   ]
 })
 export class ZoomRangeComponent implements OnInit {
@@ -11,6 +27,18 @@ export class ZoomRangeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    
+    const map = new mapboxgl.Map({
+      container: 'mapa', // container ID
+      style: 'mapbox://styles/mapbox/streets-v11', // style URL
+      center: [-75.48983051866664, 5.065707743505762], // starting position [lng, lat]
+      zoom: 18, // starting zoom
+      //projection: 'globe' // display the map as a 3D globe
+    });
+    map.on('style.load', () => {
+    map.setFog({}); // Set the default atmosphere style
+    });
+
   }
 
 }
