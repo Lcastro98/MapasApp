20c3f416d89550ff52df221e0e504c7483bca6c2
Andrea Castro Manzano <andrea.castro@sofka.local>
2022-11-03 10:41:47 -0500

    Controlar el objeto del mapa, ZoomIn y ZoomOut

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
 @Component({
@@ -22,23 +22,32 @@ import * as mapboxgl from 'mapbox-gl';
   `
   ]
 })
-export class ZoomRangeComponent implements OnInit {
+export class ZoomRangeComponent implements AfterViewInit {
 
-  constructor() { }
+  @ViewChild('mapa') divMapa!: ElementRef;
+  mapa!: mapboxgl.Map;
 
-  ngOnInit(): void {
+  constructor() { 
+    console.log('constructor', this.divMapa)
+  }
+
+  ngAfterViewInit(): void {
+    console.log('afterViewInit', this.divMapa);
     
-    const map = new mapboxgl.Map({
-      container: 'mapa', // container ID
-      style: 'mapbox://styles/mapbox/streets-v11', // style URL
-      center: [-75.48983051866664, 5.065707743505762], // starting position [lng, lat]
-      zoom: 18, // starting zoom
-      //projection: 'globe' // display the map as a 3D globe
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement, 
+      style: 'mapbox://styles/mapbox/streets-v11', 
+      center: [-75.48983051866664, 5.065707743505762], 
+      zoom: 18
     });
-    map.on('style.load', () => {
-    map.setFog({}); // Set the default atmosphere style
-    });
+  }
 
+  zoomOut() {
+    this.mapa.zoomOut();
+  }
+
+  zoomIn(){
+    this.mapa.zoomIn();
   }
 
 }
