fe66004c4c0dc8c75cd7ff93609b153c129fa78c
Andrea Castro Manzano <andrea.castro@sofka.local>
2022-11-04 16:01:15 -0500

    Borrar y actualizar marcadores

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -77,6 +77,8 @@ export class MarcadoresComponent implements AfterViewInit {
       marker: nuevoMarcador
     });
 
+    this.guardarMarcadoresLocalStorage();  
+
     nuevoMarcador.on('dragend', () => {
       this.guardarMarcadoresLocalStorage();
     });
@@ -125,7 +127,17 @@ export class MarcadoresComponent implements AfterViewInit {
         marker: newMarker,
         color: m.color
       });
-    })
+
+      newMarker.on('dragend', () => {
+        this.guardarMarcadoresLocalStorage();        
+      })
+    });
+  }
+
+  borrarMarcador(i: number) {
+    this.marcadores[i].marker?.remove();
+    this.marcadores.splice(i, 1);
+    this.guardarMarcadoresLocalStorage();
   }
 
 }
